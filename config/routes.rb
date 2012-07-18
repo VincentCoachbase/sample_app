e392fdd1b4ec6c2399e4fc5dd1c516bd7d611722
Vincent <vincent@jpluscorp.com>
2012-07-18 15:49:05 +0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 SampleApp::Application.routes.draw do
+
   root to: 'static_pages#home'
 
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/signup',  to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
